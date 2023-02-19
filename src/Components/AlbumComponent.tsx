61d4aa95bd761f0977f39c039f0f548b516e7472
Aydot <ayomidotzee@gmail.com>
2023-02-19 16:52:52 +0100

    UI for viewAlbum Completed

--- src/Components/AlbumComponent.tsx
+++ src/Components/AlbumComponent.tsx
@@ -5,7 +5,7 @@ import { FaEllipsisV } from "react-icons/fa"
 import useWindowDimensions from "../Hooks/windowDimensions"
 import SpotifyWebApi from "spotify-web-api-js"
 import { AppContext } from "../Contexts/AppContext"
-import { RiPlayListAddFill, RiPlayCircleFill, RiHeart2Fill } from 'react-icons/ri'
+import { RiPlayListAddFill, RiPlayCircleFill, RiHeart2Fill, RiHeart2Line } from 'react-icons/ri'
 import { LazyLoadImage } from "react-lazy-load-image-component"
 
 export default function AlbumComponent() {
@@ -92,47 +92,49 @@ export default function AlbumComponent() {
   
   return (
     <div>
-      <img src={data.image} className='rounded-xl h-[350px] w-full max-w-[350px]'/>
-      <div className='lg:ml-7 lg:mt-[80px]'>
-        <div className='lg:w-[500px]'>
-          <h2 className='text-[#A4C7C6] text-[32px] font-bold mt-6'>{data.name}</h2>
-          {
-            //@ts-ignore
-            data.description  && <p className='text-gray-400 text-sm'>{data.description}</p>
-          }
-          <p className='text-gray-500 text-sm mt-3'>{playlistTracks.length} Songs - 16 Hours</p>
-        </div>
-        <div className='flex justify-between mt-6 md:w-[500px] lg:w-[400px]'>
-            <div className='flex items-center cursor-pointer bg-[#424547] rounded-full px-[15px] py-[10px]'>
-              <i className='mr-2 text-yellow-500'><RiPlayCircleFill /></i>
-              <p className='text-white text-xs'>Play all</p>
-            </div>
-            <div className='flex items-center cursor-pointer bg-[#424547] rounded-full px-[15px] py-[10px]'>
-            <i className='mr-2 text-yellow-500'><RiPlayListAddFill /></i>
-              <p className='text-white text-xs'>Add to Collection</p>
-            </div>
-            <div className='flex items-center cursor-pointer bg-[#424547] rounded-full px-[15px] py-[10px]'>
-            <i className='mr-2 text-yellow-500'><RiHeart2Fill /></i>
-                <p className='text-white text-xs'>Like</p>
-            </div>
+      <div className="md:flex gap-5">
+        <img src={data.image} className='rounded-xl h-[350px] w-full max-w-[350px]'/>
+        <div className='md:mt-[100px] lg:mt-[150px]'>
+          <div className='lg:w-[500px]'>
+            <h2 className='text-[#A4C7C6] text-[32px] font-bold mt-6 md:w-[60%] lg:w-[100%]'>{data.name}</h2>
+            {
+              //@ts-ignore
+              data.description  && <p className='text-gray-400 text-sm'>{data.description}</p>
+            }
+            <p className='text-gray-500 text-sm mt-3'>{playlistTracks.length} Songs - 16 Hours</p>
+          </div>
+          <div className='flex justify-between mt-6 lg:w-[400px]'>
+              <div className='flex items-center cursor-pointer bg-[#424547] rounded-full px-[15px] py-[10px]'>
+                <i className='mr-2 text-yellow-500'><RiPlayCircleFill /></i>
+                <p className='text-white text-xs'>Play all</p>
+              </div>
+              <div className='flex items-center cursor-pointer bg-[#424547] rounded-full px-[15px] py-[10px]'>
+              <i className='mr-2 text-yellow-500'><RiPlayListAddFill /></i>
+                <p className='text-white text-xs'>Add to Collection</p>
+              </div>
+              <div className='flex items-center cursor-pointer bg-[#424547] rounded-full px-[15px] py-[10px]'>
+              <i className='mr-2 text-yellow-500'><RiHeart2Fill /></i>
+                  <p className='text-white text-xs'>Like</p>
+              </div>
+          </div>
         </div>
       </div>
       {
-        width >= 1024 ?
+        width >= 768 ?
         <div className='mt-12'>
             {playlistTracks.map(item=>{
                 return (
                     <div className='flex items-center justify-between bg-[#2c2f31] p-2 rounded-xl mb-4' key={item.id}>
                         <div className='flex items-center'>
-                            <LazyLoadImage src={item.image} className='w-[40px] h-[40px] rounded-lg'/>
-                            <img src='trackHeart.png' className='hidden lg:block ml-5'/>
+                          <LazyLoadImage src={item.image} className='w-[40px] h-[40px] rounded-lg'/>
+                          <i className='hidden md:block ml-5 text-[1.2rem]'><RiHeart2Line /></i>
                         </div>
-                        <div className='w-[300px] 2xl:w-[500px] flex justify-between'>
+                        <div className='w-[400px] lg:w-[600px] flex justify-between'>
                           <p className='text-sm text-white font-thin tracking-wide mb-0'>{item.name}</p>
-                          <p className='text-xs lg:text-sm text-white font-thin tracking-wider'>{item.artist}</p>
+                          <p className='text-xs lg:text-sm text-white font-thin tracking-wider text-end'>{item.artist}</p>
                         </div>
-                        <div className='flex items-center lg:flex-row-reverse lg:items-center'>
-                            <i className='text-yellow mb-[6px]'><FaEllipsisV /></i>
+                        <div className='flex items-center md:flex-row-reverse lg:items-center'>
+                            <i className='text-yellow-500 mb-[6px]'><FaEllipsisV /></i>
                             <p className='text-sm font-thin text-white mr-20'>{(item.duration / 1000 / 60).toFixed(2)}</p>
                         </div>
                     </div>
@@ -153,7 +155,7 @@ export default function AlbumComponent() {
                           </div>
                       </div>
                       <div className='flex-col flex items-center'>
-                          <i className='text-yellow mb-[6px]'><FaEllipsisV /></i>
+                          <i className='text-yellow-500 mb-[6px]'><FaEllipsisV /></i>
                           <p className='text-sm font-thin text-white'>{(item.duration / 1000 / 60).toFixed(2)}</p>
                       </div>
                   </div>
