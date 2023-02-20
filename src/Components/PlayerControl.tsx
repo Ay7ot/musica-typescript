c1a70168eb9290850ad2aef566c901e417a8ee27
Aydot <ayomidotzee@gmail.com>
2023-02-20 22:25:07 +0100

    Spotify player works if you change the player

--- src/Components/PlayerControl.tsx
+++ src/Components/PlayerControl.tsx
@@ -2,10 +2,9 @@ import { useContext } from "react"
 import { AppContext } from "../Contexts/AppContext"
 import SpotifyPlayer from 'react-spotify-web-playback'
 
-
 export default function PlayerControl() {
-    
-    const { dispatch, accessToken, uris } = useContext(AppContext)
+      
+  const { dispatch, accessToken, uris } = useContext(AppContext)
    
     return (
       <div className="fixed bottom-0 w-full backdrop-blur-md z-[999999] p-3">      
@@ -19,7 +18,8 @@ export default function PlayerControl() {
             color: '#FACD66',
             trackNameColor: '#EFEEE0'
           }}
-          autoPlay={true}
+          autoPlay={false}
+          play={true}
           uris={uris}
         />
     </div>
