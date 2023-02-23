ef8316f0c7524fd6b71f9bfbf67ea3078703f81e
Aydot <ayomidotzee@gmail.com>
2023-02-23 21:40:25 +0100

    Started fixing search functionality

---
+++ src/Components/ArtistInfo.tsx
@@ -0,0 +1,11 @@
+import { useLocation } from "react-router-dom"
+
+export default function ArtistInfo() {
+    const location = useLocation()
+    const artist = location.state
+    return (
+        <div>
+            
+        </div>
+    )
+}
