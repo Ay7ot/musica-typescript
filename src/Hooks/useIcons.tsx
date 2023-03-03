d63587dd6f490f7b73051f4d61dfbacf2df47022
Aydot <ayomidotzee@gmail.com>
2023-03-03 09:13:36 +0100

    Added netlify Configuration

--- src/Hooks/useIcons.tsx
+++ src/Hooks/useIcons.tsx
@@ -3,13 +3,13 @@ import { HiUser, HiHome } from 'react-icons/hi'
 import { IoLogOut } from 'react-icons/io5'
 import { TbPlaylistAdd } from 'react-icons/tb'
 import { MdFeaturedPlayList, MdRadio, MdOndemandVideo } from 'react-icons/md'
-
+import {BsFillCollectionPlayFill} from 'react-icons/bs'
 const useIcon = (iconName: string) => {
     switch (iconName) {
         case 'Home':
             return <HiHome />;
         case 'Collections' :
-            return <TbPlaylistAdd />
+            return <BsFillCollectionPlayFill />
         case 'Radio':
             return <MdRadio />
         case 'Videos': 
